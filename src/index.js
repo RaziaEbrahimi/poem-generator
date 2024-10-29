d978bab4526ca551deb1040c508289ffd3e37a97
Razia Ebrahimi <razia.ebrahimi706@gmail.com>
2024-10-29 22:19:21 +0430

    Added API

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 function generatePoem(event) {
   event.preventDefault();
+  let poemTitle = document.querySelector("#instructions");
+  let apiKey = "c0fe48db87o5af7b7dbeb543tdaa050b";
+  let prompt = `User instructions: Generate a Persian poem about ${poemTitle.value}`;
+  let context =
+    "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem in Persian and the direction should be right to left. Make sure to follow the user instructions.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  axios.get(apiUrl).then(showPoem);
+}
 
+function showPoem(response) {
   new Typewriter("#poem", {
-    strings: "نفس باد صبا مشک فشان خواهد شد",
+    strings: response.data.answer,
     autoStart: true,
-    delay: 1,
+    delay: 10,
     cursor: "",
   });
 }
-
 let poemFormElement = document.querySelector("#poem-generator-form");
 poemFormElement.addEventListener("submit", generatePoem);

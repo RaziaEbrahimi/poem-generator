514a2a04c1d6211f6080bf0de9273bdb95fe2e15
Razia Ebrahimi <razia.ebrahimi706@gmail.com>
2024-10-29 23:40:39 +0430

    Added hint

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ function generatePoem(event) {
   event.preventDefault();
   let poemTitle = document.querySelector("#instructions");
   let apiKey = "c0fe48db87o5af7b7dbeb543tdaa050b";
-  let prompt = `User instructions: Generate a Persian poem about ${poemTitle.value}`;
+  let prompt = `User instructions: Generate a English and Persian poem about ${poemTitle.value}`;
   let context =
-    "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem in Persian and the direction should be right to left. Make sure to follow the user instructions.";
+    "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem in English and Persian and follow direction for Persian poem start right to left . Make sure to follow the user instructions.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   axios.get(apiUrl).then(showPoem);
 }
